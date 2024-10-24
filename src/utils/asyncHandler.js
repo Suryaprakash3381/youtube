9bcb19709025642c46b264954d222c5073b1cc84
surya <suryaprakashsahoo4105@gmail.com>
2024-10-24 14:04:07 +0530

    change the code  /   alternate code of async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,29 +1,29 @@
-// // method 1
+// method 1
 
-// const asyncHandler = (fn) =>  async (req , res , next) => {
-//     try {
-//         await fn(req, res,next )
-//     } catch (error) {
-//       res.status(err.code || 404).json({
-//         success: false,
-//         message: error.message || "Server Error"
-//       })
-//     }
-// }
+const asyncHandler = (fn) =>  async (req , res , next) => {
+    try {
+        await fn(req, res,next )
+    } catch (error) {
+      res.status(err.code || 404).json({
+        success: false,
+        message: error.message || "Server Error"
+      })
+    }
+}
 
-// export { asyncHandler } 
+export { asyncHandler } 
 
 // metrhod 2
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-      Promise.resolve(requestHandler(req, res, next))
-          .catch((err) => {
-              res.status(err.code || 500).json({
-                  success: false,
-                  message: err.message || "Server Error"
-              });
-          });
-  };
-};
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//       Promise.resolve(requestHandler(req, res, next))
+//           .catch((err) => {
+//               res.status(err.code || 500).json({
+//                   success: false,
+//                   message: err.message || "Server Error"
+//               });
+//           });
+//   };
+// };
 
-export { asyncHandler };
+// export { asyncHandler };
